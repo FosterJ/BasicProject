4efe6df77eb2748237fb5b2ee89ea550b94e7e58
Joe <joe85foster@gmail.com>
2018-11-27 13:47:07 -0500

    Updated URL paths | Assigned use of templates

--- BasicProject/BasicProject/urls.py
+++ BasicProject/BasicProject/urls.py
@@ -8,7 +8,7 @@ import HelloDjangoApp.views
 #Django processes URL patterns in the order they appear in the array
 urlpatterns = [
     url(r'^$', HelloDjangoApp.views.index, name='index'),
-    url(r'^homes$', HelloDjangoApp.views.index, name='home')
+    url(r'^home$', HelloDjangoApp.views.index, name='home')
     ]
 
 # Uncomment the next two lines to enable the admin:
